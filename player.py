8592d3ae4fc42d02d913463db78e3bc6dc05e7ee
SaladAss <131820783+MrWillyFooFoo@users.noreply.github.com>
2024-02-07 03:58:50 -0800

    Update player.py

--- player.py
+++ player.py
@@ -1,12 +1,39 @@
+import pygame
+import random
+
+PLAYER_SIZE = 128, 128
+
+
 class Player:
 
-    def __init__(self, _name, _color, _pos):
-
+    def __init__(self, _name: str, _pos: tuple):
         self.Name = _name
 
-        self.Color = _color
+        self.x_pos = _pos[0]
 
-        self.Rect = None
+        self.y_pos = _pos[1]
+
+        self.rect = pygame.Rect((self.x_pos, self.y_pos), PLAYER_SIZE)
+
+        self.color = (random.randrange(255), random.randrange(255), random.randrange(255))
+
+        self.position = (self.x_pos, self.y_pos)
+
+    def update(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+    def update_position(self, key):
+        key = key.lower()
+        match key:
+            case "w":
+                self.y_pos = 3
+            case "a":
+                self.x_pos -= 2
+            case "s":
+                self.y_pos += 2
+            case "d":
+                self.x_pos += 2
+
 
     @property
     def name(self):
@@ -17,10 +44,7 @@ class Player:
         self.Name = value
 
     @property
-    def color(self):
-        return self.Color
+    def get_position(self):
+        return self.position
 
-    @color.setter
-    def color(self, value):
-        self.Color = value
 
