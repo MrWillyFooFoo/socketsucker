7e8c2438a8d7a0aa0f77c9aeeb49fa65ff9fbb07
SaladAss <131820783+MrWillyFooFoo@users.noreply.github.com>
2024-03-03 02:06:53 +0000

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
 import pygame
 import random
+import math
+import sympy
 
 PLAYER_SIZE = 128, 128
-SPEED = 3
+SPEED = 0.1
 
 
 class Player:
@@ -16,7 +18,7 @@ class Player:
 
         self.rect = pygame.Rect((self.x_pos, self.y_pos), PLAYER_SIZE)
 
-        self.color = (random.randrange(255), random.randrange(255), random.randrange(255))
+        self.color = (random.randrange(255), random.randrange(100), random.randrange(255))
 
     def update(self, surface):
         self.rect = pygame.Rect((self.x_pos, self.y_pos), PLAYER_SIZE)
@@ -24,9 +26,16 @@ class Player:
 
     def move(self, movement):
         hinput, vinput = movement[0], movement[1]
+        raddir = math.atan2(0 - vinput, hinput)  # Direction in radians
+        degdir = (raddir * (180 / math.pi))
+        pntdir = (degdir + 360) % 360
+        radlen = [(SPEED * math.cos(math.radians(pntdir))), (SPEED * -math.sin(math.radians(pntdir)))]
+        radlen[0] = math.degrees(radlen[0])
+        radlen[1] = math.degrees(radlen[1])
+        print(radlen[0], radlen[1])
 
-        self.x_pos += hinput * SPEED
-        self.y_pos += vinput * SPEED
+        self.x_pos += radlen[0]
+        self.y_pos += radlen[1]
 
     def player_input(self, key):
         pass
