b311924ec57fd68e6f2143bdae0b0b501a5bf973
SaladAss <131820783+MrWillyFooFoo@users.noreply.github.com>
2024-03-05 04:42:17 -0800

    Update server.py
    changed layout, fixed some connection issue and did smth else i forgot

--- server.py
+++ server.py
@@ -3,17 +3,23 @@ import threading
 import pickle
 
 # Server Constants
+
 HEADER = 16
 PORT = 5050  # This is the port number
 SERVER = socket.gethostbyname(socket.gethostname())  # This gets my local IPV4 Address
-ADDR = (SERVER, PORT)  # When we bind our socket to a specific address it needs to be in a tuple
+ADDR = (
+    SERVER,
+    PORT,
+)  # When we bind our socket to a specific address it needs to be in a tuple
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECTED"
 NAME_REQ = "!NAME:"
 MOVE_REQ = "!MOVE"
-ADDCHAR_REQ = "!ADDCHAR"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # What type of addresses we are looking for
+server = socket.socket(
+    socket.AF_INET,         # What type of addresses we are looking for
+    socket.SOCK_STREAM
+    )
 server.bind(ADDR)  # Binding the address to the socket
 
 # Game constants
@@ -21,8 +27,6 @@ PLAYER_SIZE = 128
 S_WIDTH, S_HEIGHT = 1200, 800
 FPS = 60
 
-
-# Listening
 def handle_client(conn, addr):
     print(f"[New connection] {addr} connected, assigning a rect.")
 
@@ -43,25 +47,22 @@ def handle_client(conn, addr):
             except:
                 msg = pickle.loads(data)
                 obj = True
-
             if not obj:  # If msg was not an object
 
                 if msg == DISCONNECT_MESSAGE:
 
                     connected = False
-
+                    print(f"Player disconnected, : {threading.active_count() - 1}")
                 elif NAME_REQ in msg:
 
-                    client_name = (msg[len(NAME_REQ)::])
+                    client_name = msg[len(NAME_REQ) : :]
 
                     send(client_name, conn)
 
                     print(client_name)
             else:
                 dataclass = type(msg)
-
             print(f"{addr} said: {msg}")
-
     conn.close()
 
 
@@ -78,7 +79,7 @@ def start():  # Used to start listening to connections
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()  # This line will wait, for a connection, then store the addr and port
+        conn, addr = (server.accept())  # This line will wait, for a connection, then store the addr and port
         print(conn, addr)
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
